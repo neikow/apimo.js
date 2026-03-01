78378b5a7f89f18bf6e169554bdbe17c6325a894
vitaly <50383391+Neikow@users.noreply.github.com>
2026-03-01 16:23:37 +0100

    fix types

--- src/schemas/internal.ts
+++ src/schemas/internal.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod'
 
-export const TYPE_UNDOCUMENTED = z.unknown().transform((v, { path }) => {
-  console.warn(`Unhandled \`${path}\` with value \`${v}\``)
+export const TYPE_UNDOCUMENTED = z.unknown().transform((v) => {
+  console.warn(`Unhandled undocumented field with value \`${v}\``)
   return v
 })
 
-export const TYPE_UNDOCUMENTED_NULLABLE = z.unknown().nullable().transform((v, { path }) => {
+export const TYPE_UNDOCUMENTED_NULLABLE = z.unknown().nullable().transform((v) => {
   if (v !== null) {
-    console.warn(`Unhandled \`${path}\` with value \`${v}\``)
+    console.warn(`Unhandled undocumented field with value \`${v}\``)
   }
   return v
 })
